3c5d63e832f4794a97453b9a87047f64cf9cab46
MarcoBlzz <U201922326@upc.edu.pe>
2025-05-08 18:41:22 -0500

    Delete MetroFast/MetroFast directory

--- MetroFast/MetroFast/src/main/java/pe/edu/upc/metrofast/repositories/IHistorialDeViajesRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.metrofast.repositories;
-
-import pe.edu.upc.metrofast.entities.HistorialDeViajes;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IHistorialDeViajesRepository extends JpaRepository<HistorialDeViajes, Integer> {
-}
