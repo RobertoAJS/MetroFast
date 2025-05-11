fef5e34e7191d9221cd63fef86db54fc310d9dbb
Brandon256star <U20201E762@upc.edu.pe>
2025-05-11 04:28:17 -0500

    new commit

--- src/main/java/pe/edu/upc/metrofast/repositories/ISuscripcionRepository.java
+++ src/main/java/pe/edu/upc/metrofast/repositories/ISuscripcionRepository.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.metrofast.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.upc.metrofast.entities.Suscripcion;
+
+import java.util.List;
 
 public interface ISuscripcionRepository extends JpaRepository<Suscripcion, Integer> {
 
