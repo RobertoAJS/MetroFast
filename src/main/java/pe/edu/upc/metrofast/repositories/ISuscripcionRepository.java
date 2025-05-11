f22120992c82a2b5fee7722df424b3bb9f5301b3
Brandon256star <U20201E762@upc.edu.pe>
2025-05-11 04:32:53 -0500

    new commit

--- src/main/java/pe/edu/upc/metrofast/repositories/ISuscripcionRepository.java
+++ src/main/java/pe/edu/upc/metrofast/repositories/ISuscripcionRepository.java
@@ -9,7 +9,13 @@ import java.util.List;
 
 public interface ISuscripcionRepository extends JpaRepository<Suscripcion, Integer> {
 
+    // Query de suscripciones de busqueda de Usuario
+    @Query("SELECT s FROM Suscripcion s WHERE s.status = true AND s.usuarioid.userid = :userId")
+    List<Suscripcion> findActiveSubscriptionsByUserId(@Param("userId") int userId);
 
+    // Query de suscripciones que expiran durante este mes
+    @Query ("SELECT s FROM Suscripcion s WHERE MONTH(s.enddate) = MONTH(CURRENT_DATE) AND YEAR(s.enddate) = YEAR(CURRENT_DATE)")
+    List<Suscripcion> findSubscriptionsExpiringThisMonth();
 
 
 }
